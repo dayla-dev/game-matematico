3a26de8d535d15905b523a9442603d249889fed2
Dayla <dayla.rdg3@gmail.com>
2018-12-02 09:45:44 -0300

    camionete passageiros

--- src/br/com/locadoraPBD/model/beans/CamionetePassageiros.java
+++ src/br/com/locadoraPBD/model/beans/CamionetePassageiros.java
@@ -1,6 +1,7 @@
 package br.com.locadoraPBD.model.beans;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,118 @@ public class CamionetePassageiros implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private boolean airbag;
+    private boolean direcaoAssistida;
+    private boolean cintoSeg;
+    private boolean rodasLigaLeve;
+    private boolean contPoluicaoAr;
 
-   
+    public CamionetePassageiros() {
+    }
+
+    public CamionetePassageiros(boolean airbag, boolean direcaoAssistida, boolean cintoSeg, boolean rodasLigaLeve, boolean contPoluicaoAr) {
+        this.airbag = airbag;
+        this.direcaoAssistida = direcaoAssistida;
+        this.cintoSeg = cintoSeg;
+        this.rodasLigaLeve = rodasLigaLeve;
+        this.contPoluicaoAr = contPoluicaoAr;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + (this.airbag ? 1 : 0);
+        hash = 59 * hash + (this.direcaoAssistida ? 1 : 0);
+        hash = 59 * hash + (this.cintoSeg ? 1 : 0);
+        hash = 59 * hash + (this.rodasLigaLeve ? 1 : 0);
+        hash = 59 * hash + (this.contPoluicaoAr ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CamionetePassageiros other = (CamionetePassageiros) obj;
+        if (this.airbag != other.airbag) {
+            return false;
+        }
+        if (this.direcaoAssistida != other.direcaoAssistida) {
+            return false;
+        }
+        if (this.cintoSeg != other.cintoSeg) {
+            return false;
+        }
+        if (this.rodasLigaLeve != other.rodasLigaLeve) {
+            return false;
+        }
+        if (this.contPoluicaoAr != other.contPoluicaoAr) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CamionetePassageiros{" + "id=" + id + ", airbag=" + airbag + ", direcaoAssistida=" + direcaoAssistida + ", cintoSeg=" + cintoSeg + ", rodasLigaLeve=" + rodasLigaLeve + ", contPoluicaoAr=" + contPoluicaoAr + '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isAirbag() {
+        return airbag;
+    }
+
+    public void setAirbag(boolean airbag) {
+        this.airbag = airbag;
+    }
+
+    public boolean isDirecaoAssistida() {
+        return direcaoAssistida;
+    }
+
+    public void setDirecaoAssistida(boolean direcaoAssistida) {
+        this.direcaoAssistida = direcaoAssistida;
+    }
+
+    public boolean isCintoSeg() {
+        return cintoSeg;
+    }
+
+    public void setCintoSeg(boolean cintoSeg) {
+        this.cintoSeg = cintoSeg;
+    }
+
+    public boolean isRodasLigaLeve() {
+        return rodasLigaLeve;
+    }
+
+    public void setRodasLigaLeve(boolean rodasLigaLeve) {
+        this.rodasLigaLeve = rodasLigaLeve;
+    }
+
+    public boolean isContPoluicaoAr() {
+        return contPoluicaoAr;
+    }
+
+    public void setContPoluicaoAr(boolean contPoluicaoAr) {
+        this.contPoluicaoAr = contPoluicaoAr;
+    }
 }
