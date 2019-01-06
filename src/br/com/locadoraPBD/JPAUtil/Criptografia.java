bbc6922a683ae4020d96f05b1b6fe83569ed2d54
Dayla <dayla.rdg3@gmail.com>
2019-01-06 02:30:43 -0300

    ajeitando a descriptografia

--- src/br/com/locadoraPBD/JPAUtil/Criptografia.java
+++ src/br/com/locadoraPBD/JPAUtil/Criptografia.java
@@ -3,6 +3,7 @@ package br.com.locadoraPBD.JPAUtil;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,19 +12,19 @@ import java.util.logging.Logger;
  * @author Dayla
  */
 public class Criptografia {
+    
+    private static String mensagem;
      
-       public static String encriptografar(String senha){
+    public static String encriptografar(String senha){
         
-         String retorno = "";
-         MessageDigest md;
+        mensagem = Base64.getEncoder().encodeToString(senha.getBytes());
         
-         try{
-             md = MessageDigest.getInstance("MD5");
-             BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
-             retorno = hash.toString(16);
-         }
-         catch(Exception e){}
-         
-         return retorno;
-       }
+        return mensagem;
+    }
+    public static void descriptografar(String senha){
+        
+        byte[] arrayByteDescriptografado = Base64.getDecoder().decode(mensagem);
+        senha = new String(arrayByteDescriptografado);
+        
+    }
 }
